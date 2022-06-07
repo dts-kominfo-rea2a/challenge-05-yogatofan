37dbfaceffb046b9c795395f759f56e9289d10bd
Yoga Ari Tofan <y.aritofan@gmail.com>
2022-06-07 13:58:40 +0700

    challenge: JS Callback

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (daftarNama, cbSorting) => {
+  const result = [];
+  const data = cbSorting(daftarNama);
+
+  for (let index = 0; index < data.length; index++) {
+    let no = index + 1;
+    const item = data[index];
+
+    result.push(no + ". " + item);
+  }
+
+  return result;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (daftarNama) => {
+  return daftarNama.sort();
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (daftarNama) => {
+  return daftarNama.sort().reverse();
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
